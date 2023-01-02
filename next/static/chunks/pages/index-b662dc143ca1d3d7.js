78c7147ee4e62f9d1f649f2254d8f34ba688e45e
Web Developer Services <80251513+webdev-service@users.noreply.github.com>
2023-01-02 13:12:39 +0800

    Add files via upload

--- next/static/chunks/pages/index-b662dc143ca1d3d7.js
+++ next/static/chunks/pages/index-b662dc143ca1d3d7.js
@@ -81,7 +81,7 @@
                                                 children: [(0, n.jsx)("span", {
                                                     className: "icon",
                                                     children: (0, n.jsx)("img", {
-                                                        src: "svg/nft.svg",
+                                                        src: "svg/robot.svg",
                                                         alt:"",
                                                         className: "fn__svg"
                                                     })
@@ -173,7 +173,7 @@
                                                 children: [(0, n.jsx)("span", {
                                                     className: "icon",
                                                     children: (0, n.jsx)("img", {
-                                                        src: "svg/nft.svg",
+                                                        src: "svg/robot.svg",
                                                         alt:"",
                                                         className: "fn__svg"
                                                     })
